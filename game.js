4558d29b33e0c4afa4e3c196d9b298e0b3eaad60
Sadeeq Shittu <soshittu@I47061W53510194.instr.cps.k12.il.us>
2015-10-01 16:52:20 -0500

    loaded the assets to game.js

--- game.js
+++ game.js
@@ -26,7 +26,12 @@ var game = new Phaser.Game(600, 600, Phaser.AUTO, gameCanvas,{ preload: preload,
 	 */                        
 function preload() {
     // load all game assets here
+  game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
+    
 } 
 
 	/**
@@ -37,7 +42,7 @@ function preload() {
 	 */
 function create() {
   // create the game world here
-    
+    game.add.sprite(0, 0 , 'star');
 }
   
 	/**
